73b4ead8fa865aa642f9925e2a63c2c20bab7c7a
Baldy76 <rgleech76@gmail.com>
2026-04-07 15:33:38 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,24 @@
 
+const cacheName = 'joinery-v1';
+const staticAssets = [
+  './',
+  './index.html',
+  './app.js',
+  './manifest.json'
+];
+
+self.addEventListener('install', async e => {
+  const cache = await caches.open(cacheName);
+  await cache.addAll(staticAssets);
+  return self.skipWaiting();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    (async () => {
+      const r = await caches.match(e.request);
+      if (r) return r;
+      return fetch(e.request);
+    })()
+  );
+});
